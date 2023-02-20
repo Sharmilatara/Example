586458b6d9d6f78a0b3abdce17478b305a69415b
Sharmilatara <105544178+Sharmilatara@users.noreply.github.com>
2023-02-20 19:00:16 +0530

    Add files via upload

---
+++ DIS/src/test/java/testRunner/JunitTest.java
@@ -0,0 +1,13 @@
+package testRunner;
+
+import org.junit.Test;
+
+public class JunitTest {
+	@Test
+	public void test1() {
+		System.out.println("Test 1");
+		System.out.println("test check");
+		
+	}
+
+}
